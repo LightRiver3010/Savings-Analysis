099fa0d8463510c792a49c246ac6bf86da2ecd03
Jimmy <savarynj@gmail.com>
2026-01-28 16:23:24 -0500

    added emojis to initial page (more decor to come soon)

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("Savings Simulator Demo")
+st.title("Savings Simulator Demo :money_with_wings:")
 st.subheader("Welcome to my Savings Demo!")
 st.write("In this demo, you will:")
-st.markdown("1. Algin yourself with a customer archetype that best matches your spending patterns\n2. Proceed through the savings sim as that customer, seeing current expenses & areas for savings\n3. Learn tips for saving money based on your customer's highest expenses\n4. See how much those tips can save you each month")
-st.markdown("If that sounds good to you, then *press the button below to begin!*")
+st.markdown("1. Algin yourself with a customer archetype that **best matches your spending patterns** :white_check_mark: \n 2. Proceed through the savings sim as that customer, seeing current expenses & areas for savings :white_check_mark: \n3. **Learn tips for saving money** based on your customer's highest expenses :white_check_mark: \n4. **See how much those tips can save you** each month :white_check_mark: ")
+st.markdown("If that sounds good to you, then :point_down:*press the button below to begin!* :point_down:")
 if st.button("Begin"):
     st.switch_page("pages/archetypes.py")
     
